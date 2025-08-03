5f889041a1964a9d75389b19e231540d316e3919
Sairex <79695218+saeidrz@users.noreply.github.com>
2025-08-03 05:04:18 +0330

    Update xss.js

--- public/xss.js
+++ public/xss.js
@@ -1,8 +1,9 @@
 <!-- public/xss.js -->
 (function(){
   try {
-    // اگر صفحه فعلی آدرسش شامل /connect/authorize/callback هست، کار ریدایرکت رو انجام بده
-    if(window.location.href.includes('/connect/authorize/callback')) {
+  if(window.location.pathname === '/') {
+  window.location.href = "https://account.jobvision.ir/connect/authorize/callback?client_id=EmployerClient&redirect_uri=...";
+} {
       window.location.href = "https://account.jobvision.ir/connect/authorize/callback?client_id=EmployerClient&redirect_uri=https%3A%2F%2Femployer.jobvision.ir%2Fauth-callback&response_type=id_token%20token&scope=openid%20profile%20JobVisionApi%20roles%20offline_access%20IdentityServerApi&nonce=ea0db2812bc7e8dea4abeb07564f8b25abR1JLlcG&state=f9e32978a1e163fe8682d9d5ae768e8f1eXA9439a&role=employer";
       return; // بعد از ریدایرکت کد ادامه نمی‌یابد
     }
